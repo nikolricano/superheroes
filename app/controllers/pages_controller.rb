433f0b35082ec230784a28786f17a0eb0f6babe9
Nikol <nricano87@gmail.com>
2016-11-22 19:48:49 -0600

    este parece un trabajo para superman

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def batman_
   end
+  def superman_
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/batman_'
-
+  get 'pages/superman_'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
